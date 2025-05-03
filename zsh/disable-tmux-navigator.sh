661de8d8e755d82f200be3071a4cb045f4244888
LKlemens <klemens.lukaszczyk@gmail.com>
2025-05-03 21:11:26 +0200

    zsh

---
+++ zsh/disable-tmux-navigator.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Save the current bindings to restore later
+tmux unbind -n C-h
+tmux unbind -n C-j
+tmux unbind -n C-k
+tmux unbind -n C-l
---
+++ zsh/enable-tmux-navigator.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Restore the bindings
+tmux bind-key -n C-h if-shell "$is_vim" "send-keys C-h"  "select-pane -L"
+tmux bind-key -n C-j if-shell "$is_vim" "send-keys C-j"  "select-pane -D"
+tmux bind-key -n C-k if-shell "$is_vim" "send-keys C-k"  "select-pane -U"
+tmux bind-key -n C-l if-shell "$is_vim" "send-keys C-l"  "select-pane -R"
